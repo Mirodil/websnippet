1730cd515a7e9f77411a29c0da1397f41be934b5
Mirodil <Mirodil@users.noreply.github.com>
2022-05-13 21:07:11 +0000

    deploy: a73eb9a7b6823a13a88285557a4929832265e775

--- _next/static/6y1f-iwx5oV9XXIihIjGJ/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fabout","\u002Fcontact","\u002Ftools-and-resources","\u002Fcategories\u002F[category]","\u002Fcategories\u002F[category]\u002Fpage\u002F[page]","\u002Fpage\u002F[page]","\u002Ftags\u002F[tag]","\u002Ftags\u002F[tag]\u002Fpage\u002F[page]","\u002Fpost\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
